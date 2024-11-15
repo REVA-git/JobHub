b66abf4f55e4c65a1bad2c23de968db833bf57cf
Shreyas M <shreyas.m246418@gmail.com>
2024-11-15 12:34:45 +0530

    first commit

---
+++ src/utils/config.js
@@ -0,0 +1,4 @@
+export const GITHUB_CLIENT_ID = 'your_github_client_id';
+export const API_BASE_URL = 'your_render_server_url' //ex:-'https://job-ui-server.onrender.com';
+export const GITHUB_REPO_URL = 'https://api.github.com/repos/your_github_username/your_github_repo_name'; //ex:-''https://api.github.com/repos/Shreyas-M-246418/job-ui-server'
+export const GITHUB_CALLBACK_URL = 'https://your_vercel_app_url/auth/github/callback'; //ex:- 'https://job-ui-six.vercel.app/auth/github/callback'
